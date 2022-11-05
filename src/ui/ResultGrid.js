334efe65ee8488e7f204bf4ae987efd720fd7380
Christian Ramirez <christian.r0602@gmail.com>
2022-11-05 12:06:50 -0500

    title and lisitng name css

--- src/ui/ResultGrid.js
+++ src/ui/ResultGrid.js
@@ -12,11 +12,12 @@ export default function ResultGrid(props) {
     <div className='contain'>
       {
     newArrWNorepets.map((result, index) => {
-       return <div className="card" style={{width: '18rem'}} key={index}>
+       return <div className="card" key={index}>
       <img className="card-img-top" src={result.images[0]} alt={result.id}></img>
-      <p className='card-text' >{result.listingName}</p>
-      <p className='card-text' >{result.price} * ${(props.days * (parseFloat(result.price.slice(1))))} total </p>
-      <p>{result.beds} bed and {result.bathrooms} bathroom</p>
+      <p className='card-text listingName' >{result.title}</p>
+      <p className='card-text'>{result.listingName}</p>
+      <p className='card-text' ><span className='priceperNight'>{result.price}</span>/night &#8226; <span className='totalPrice'>${(props.days * (parseFloat(result.price.slice(1))))} Total</span> </p>
+      <p className='card-text'>{result.beds} bed and {result.bathrooms} bathroom</p>
      </div>
 })}
 </div>
